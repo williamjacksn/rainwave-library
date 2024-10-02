5b88fbe39277bb92fabf983a67ad37ffadcd1a2e
William Jackson <565174+williamjacksn@users.noreply.github.com>
2024-10-02 16:02:34 -0500

    Stand-alone scripts for finding orphan files

---
+++ find-disabled-files.py
@@ -0,0 +1,14 @@
+import pathlib
+import rainwave_library.models
+
+# RW_CNX=postgres://user:pass@server/db python find-disabled.py
+
+cnx = rainwave_library.models.rainwave.cnx
+
+known_filenames = rainwave_library.models.rainwave.get_song_filenames(cnx)
+print(f'{len(known_filenames)} known filenames')
+
+for f in rainwave_library.models.mp3.yield_all(pathlib.Path('/icecast')):
+    sf = str(f)
+    if sf in known_filenames and not known_filenames.get(sf):
+        print(str(f))
---
+++ find-orphans.py
@@ -0,0 +1,25 @@
+import pathlib
+import rainwave_library.models
+
+# RW_CNX=postgres://user:pass@server/db python find-orphans.py
+
+cnx = rainwave_library.models.rainwave.cnx
+
+known_filenames = rainwave_library.models.rainwave.get_song_filenames(cnx)
+print(f'{len(known_filenames)} known filenames')
+
+for f in rainwave_library.models.mp3.yield_all(pathlib.Path('/icecast')):
+    sf = str(f)
+    if sf in known_filenames \
+        or sf.startswith('/icecast/xmas') \
+        or sf.startswith('/icecast/podcast') \
+        or sf.startswith('/icecast/~autoremoved') \
+        or sf.startswith('/icecast/removed') \
+        or sf.startswith('/icecast/~upcoming') \
+        or sf.startswith('/icecast/metalgear') \
+        or sf.startswith('/icecast/V-Wave Theme') \
+        or sf.startswith('/icecast/staging') \
+        or sf.startswith('/icecast/~misc') \
+        or sf.startswith('/icecast/silence'):
+        continue
+    print(str(f))
